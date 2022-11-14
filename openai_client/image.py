b97ddb101524f5d035f18ddf4aed75bad4eee714
Tou7 <houj0411@gmail.com>
2022-11-14 13:28:02 +0800

    [openai-clinet] add a wget subprocess to automatically download the images generated by DALLE-2

--- openai_client/image.py
+++ openai_client/image.py
@@ -1,15 +1,28 @@
 import os
+import subprocess
 import openai
 
-with open("private/mykey.txt", 'r') as reader:
+with open("exp/mykey.txt", 'r') as reader:
     openai.api_key = reader.read().strip()
 
-prompt_string = "worker intellengence"
+# prompt_string = "worker intellengence"
+# prompt_string = "Why Don't You Ask The Magic Conch"
+# prompt_string = "SpongeBob Asking The Magic Conch"
+# prompt_string = "SpongeBob and a large purple conch"
+prompt_string = "SpongeBob Jojo Stand"
 
 response = openai.Image.create(
-  prompt=prompt_string,
-  n=2,
-  size="1024x1024"
+    prompt=prompt_string,
+    n=3,
+    size="1024x1024"
 )
 
 print(response)
+data = response["data"]
+
+for idx, datum in enumerate(data):
+    url = datum['url']
+    filename = f"exp/img-{idx}.png"
+    print(idx)
+    # use wget to download image from url
+    subprocess.run(["wget", "-O", filename, url])
